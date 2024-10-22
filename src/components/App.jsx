9f17f09fb544a9c1b0b5ae0740639f2c0e7cfcf0
Igor Tymchuk <167434833+Igor-Tymchuk@users.noreply.github.com>
2024-10-22 10:34:13 +0300

    all done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 
 import userData from "../userData.json";
 import Profile from "./Profile/Profile";
+import friends from "../friends.json";
+import FriendList from "./FriendList/FriendList";
+import transactions from "../transactions.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 
 
 const App = () => {
@@ -13,7 +17,8 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
-      
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
